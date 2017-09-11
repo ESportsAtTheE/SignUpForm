9f842c881cfc6f7151b2d1587df4c3f3037548ac
ESportsAtTheE <31860783+ESportsAtTheE@users.noreply.github.com>
2017-09-11 17:17:47 -0400

    Fix..?

--- Index.php
+++ Index.php
@@ -5,7 +5,7 @@
     $password = $_POST['password'];
     $uname = $_POST['uname']
     $phone = $_POST['phone']
-     $from = 'From: JebBesecker'; 
+     $from = 'From: esportsatthee@gmail.com'; 
     $to = 'esportsatthee@gmail.com'; 
     $subject = 'Sign Up Information';
     $body = "From: $name\n E-Mail: $email\n TeamName: $password\n Game Name: $uname\n Phone: $phone" ;
